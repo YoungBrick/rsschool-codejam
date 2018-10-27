e9d9e5b3e436749cc405f9ecd86bda41a260cf5f
YoungBrick <tidorka@gmail.com>
2018-10-28 01:03:57 +0300

    feat: add eslint; refactor: make.js, sumOfOther.js

--- make.js
+++ make.js
@@ -1,10 +1,9 @@
 module.exports = function make(...args) {
   if (typeof args[args.length - 1] !== 'function') {
-      return function(...newArgs) {
-          return make(...args, ...newArgs)
-      }
-  } else {
-      let fn = args.pop();
-      return args.reduce((a, v) => fn(a, v), 0)
-  }   
-}
+    return function (...newArgs) {
+      return make(...args, ...newArgs);
+    };
+  }
+  const fn = args.pop();
+  return args.reduce((a, v) => fn(a, v), 0);
+};
--- sumOfOther.js
+++ sumOfOther.js
@@ -1,4 +1,4 @@
 module.exports = function sumOfOther(arr) {
-    let sum = arr.reduce((acc, v) => acc + v, 0);
-    return arr.map(v => sum - v)
-}
+  const sum = arr.reduce((acc, v) => acc + v, 0);
+  return arr.map(v => sum - v);
+};
